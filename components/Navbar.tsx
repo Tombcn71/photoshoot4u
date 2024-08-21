bf20e0a957f1f5f499ca9cb6f4dc105cc56b42f9
Tom <tbvanreijn@gmail.com>
2024-08-21 19:25:01 +0200

    nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,13 @@
-import { AvatarIcon } from "@radix-ui/react-icons";
+import { AvatarIcon, Pencil1Icon } from "@radix-ui/react-icons";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import {
+  MenuIcon,
+  CreditCard,
+  ShieldQuestion,
+  RssIcon,
+  MailIcon,
+} from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -14,6 +21,15 @@ import { Button } from "./ui/button";
 import React from "react";
 import { Database } from "@/types/supabase";
 import ClientSideCredits from "./realtime/ClientSideCredits";
+import Image from "next/image";
+import {
+  Sheet,
+  SheetContent,
+  SheetClose,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 export const dynamic = "force-dynamic";
 
@@ -35,33 +51,43 @@ export default async function Navbar() {
     .single();
 
   return (
-    <div className="flex w-full px-4 lg:px-40 py-4 items-center border-b text-center gap-8 justify-between">
-      <div className="flex gap-2 h-full">
-        <a href="/">
+    <div className=" flex w-full px-4 lg:px-40 py-4  border-b text-center gap-8 justify-between ">
+      <div className="flex gap-2 h-full ">
+        <Link href="/">
           <h2 className=" font-medium  text-2xl items-center   flex">
-            <img src="/logo7.png" width={30} height={30} alt="Logo" />{" "}
-            <span className="mt-1 ml-1 pb-1 px-2"> AI Fotosessie app</span>{" "}
+            <Image src="/logo7.png" width={30} height={30} alt="Logo" />{" "}
+            <span className="mt-1 ml-1 pb-1 px-2"> Sesiondefotos.ai</span>{" "}
           </h2>
-        </a>
+        </Link>
+      </div>
+      <div className="md:block hidden  lg:ml-auto align-items: flex-end; flex justify-end">
+        <Link href="/#Precio">
+          <Button variant={"ghost"}>Prijs</Button>
+        </Link>
+        <Link href="/#Faq ">
+          <Button variant={"ghost"}>Faq</Button>
+        </Link>
+        <Link href="/contacto">
+          <Button variant={"ghost"}>Contact</Button>
+        </Link>{" "}
       </div>
       {user && (
-        <div className="hidden lg:flex flex-row gap-2">
-          <Link href="/overview">
-            <Button variant={"ghost"}>Home</Button>
-          </Link>
+        <div className=" flex flex-row lg:ml-auto  gap-2 ">
           {stripeIsConfigured && (
             <Link href="/get-credits">
-              <Button variant={"ghost"}>Get Credits</Button>
+              <Button variant={"ghost"}>koop credits</Button>
             </Link>
           )}
         </div>
       )}
-      <div className="flex gap-4 lg:ml-auto">
+      <div className=" flex gap-4 lg:ml-auto scroll-smooth">
         {!user && (
-          <Link href="/login">
-            <Button variant={"ghost"}>Login / Signup</Button>
+          <Link href="/login" className="md:block hidden">
+            <Button className="text-white bg-sky-600 hover:bg-sky-700 w-full border rounded-full">
+              Iniciar sesión / Registrarse{" "}
+            </Button>
           </Link>
-        )}
+        )}{" "}
         {user && (
           <div className="flex flex-row gap-4 text-center align-middle justify-center">
             {stripeIsConfigured && (
@@ -69,7 +95,7 @@ export default async function Navbar() {
             )}
             <DropdownMenu>
               <DropdownMenuTrigger asChild className="cursor-pointer">
-                <AvatarIcon height={24} width={24} className="text-primary" />
+                <AvatarIcon height={24} width={24} className="text-sky-600" />
               </DropdownMenuTrigger>
               <DropdownMenuContent className="w-56">
                 <DropdownMenuLabel className="text-primary text-center overflow-hidden text-ellipsis">
@@ -81,13 +107,63 @@ export default async function Navbar() {
                     type="submit"
                     className="w-full text-left"
                     variant={"ghost"}>
-                    Log out
+                    Uitloggen
                   </Button>
                 </form>
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
-        )}
+        )}{" "}
+        <Sheet>
+          <div className="bg-white md:hidden ">
+            <SheetTrigger>
+              <MenuIcon className="mr-2 mt-3" />
+            </SheetTrigger>
+            <div className="bg-white">
+              <SheetContent className="w-[220px] h-[250px] p-1 text-align: leftsm:w-[240px]">
+                {" "}
+                <div className="overflow-anchor: none;">
+                  <ul>
+                    <li className="flex flex-col pt-4   ">
+                      {" "}
+                      <Link href="/prijs" className="items-cemter">
+                        <Button
+                          variant={"ghost"}
+                          className="text-l	text-align: left">
+                          Prijs
+                        </Button>
+                      </Link>
+                      <Link href="/faq">
+                        <Button variant={"ghost"} className="text-l	">
+                          Faq
+                        </Button>
+                      </Link>
+                      <Link href="/blog">
+                        <Button variant={"ghost"} className="text-l	">
+                          Blog
+                        </Button>
+                      </Link>{" "}
+                      <Link href="/contact">
+                        <Button variant={"ghost"} className="text-l	">
+                          Contact
+                        </Button>{" "}
+                      </Link>{" "}
+                      <SheetClose asChild>
+                        {!user && (
+                          <Link href="/login" className="pl-4 pt-4">
+                            <Button className="bg-sky-600 hover:bg-sky-700 pl-4">
+                              Inloggen / Registreren
+                            </Button>
+                          </Link>
+                        )}
+                      </SheetClose>
+                    </li>
+                  </ul>
+                </div>{" "}
+              </SheetContent>{" "}
+            </div>
+          </div>
+        </Sheet>
       </div>
     </div>
   );
