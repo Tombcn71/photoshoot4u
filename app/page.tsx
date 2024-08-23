92a3128c8d67d7b23030ef5ea667f8daf3c6f9ec
Tom <tbvanreijn@gmail.com>
2024-08-23 08:56:24 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -27,13 +27,13 @@ export default async function Index() {
       <div className="flex flex-col lg:flex-row items-center gap-8 p-8 max-w-6xl w-full">
         <div className="flex flex-col space-y-4 lg:w-1/2 w-full">
           <h1 className="text-5xl font-bold ">
-            ✨Professionele portretfoto's maken met AI
+            Professionele portretfoto's maken met ✨AI
             <br />
-            zonder studio of fotograaf
           </h1>
           <p className="text-gray-600 text-xl">
             👍Ideaal voor sociale profielen, websites, cv's en professionele
-            portfolio's, makkelijk vanaf thuis of je werk!
+            portfolio's, zonder studio of fotograaf, makkelijk vanaf thuis of je
+            werk!
           </p>
           <div className="flex flex-col space-y-2">
             <Link href="/login">
