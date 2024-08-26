35d9197c865f91468cecc59d3116c8ec594a5c46
Tom <tbvanreijn@gmail.com>
2024-08-26 09:41:23 +0200

    pricing table

--- components/stripe/StripeTable.tsx
+++ components/stripe/StripeTable.tsx
@@ -1,27 +1,31 @@
-'use client'
-import { User } from '@supabase/supabase-js';
-import React, { useEffect } from 'react';
+"use client";
+import { User } from "@supabase/supabase-js";
+import React, { useEffect } from "react";
 
-interface StripePricingTableProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
-  'pricing-table-id': string;
-  'publishable-key': string;
+interface StripePricingTableProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLElement>,
+    HTMLElement
+  > {
+  "pricing-table-id": string;
+  "publishable-key": string;
 }
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'stripe-pricing-table': StripePricingTableProps;
+      "stripe-pricing-table": StripePricingTableProps;
     }
   }
 }
 
 type Props = {
   user: User;
-}
+};
 
 const StripePricingTable = ({ user }: Props) => {
   useEffect(() => {
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.src = "https://js.stripe.com/v3/pricing-table.js";
     script.async = true;
 
@@ -29,20 +33,18 @@ const StripePricingTable = ({ user }: Props) => {
 
     return () => {
       document.body.removeChild(script);
-    }
+    };
   }, []);
 
   return (
-    <div className='flex flex-1 flex-col w-full'>
+    <div className="flex flex-1 flex-col w-full">
       <stripe-pricing-table
-          pricing-table-id="prctbl_1P0TL0C3ic5Sd20TGpWOU2Fi"
-          publishable-key="pk_live_51P0SikC3ic5Sd20T9QRaRKIkqy8l951LDgeOxcP24ZRXHnQzjnOFM7tfhsYdWksn1wNBdejJzvaxXGq0yRAxm14A00Py0XreGk"
-          client-reference-id={user.id}
-          customer-email={user.email}
-      >
-      </stripe-pricing-table>
+        pricing-table-id="prctbl_1PJFjqHo2N94V3VmyGWx5TBQ"
+        publishable-key="pk_live_51PJEz5Ho2N94V3Vm1FbJtVJmYNO6sV4vMb3VnILPGQ7GiIMO6fibByCzaUHY2I0eaI2aOKqvKui3LrGAV8uPluuc00WzR4hqjb"
+        client-reference-id={user.id}
+        customer-email={user.email}></stripe-pricing-table>
     </div>
   );
-}
+};
 
 export default StripePricingTable;
