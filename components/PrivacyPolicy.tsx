554b6fe46f363147079a6053951508d3f189562f
Tom <tbvanreijn@gmail.com>
2024-09-15 20:59:49 +0200

    text

--- components/PrivacyPolicy.tsx
+++ components/PrivacyPolicy.tsx
@@ -20,38 +20,38 @@ export const PrivacyPolicy = () => {
           Applicatiegebruikgegevens Apparaatinformatie{" "}
         </p>{" "}
         <p>
-          3. Gegevensgebruik We gebruiken uw gegevens om: De app te leveren en
+          3. Gegevensgebruik: We gebruiken uw gegevens om: De app te leveren en
           te onderhouden Uw account te beheren U te informeren over updates en
           nieuwe functies Uw gebruik van de app te analyseren en te verbeteren
           Uw hoofdfoto's te genereren{" "}
         </p>
         <p>
-          4. Gegevensdeling We delen uw persoonlijke gegevens niet met derden,
+          4. Gegevensdeling: We delen uw persoonlijke gegevens niet met derden,
           tenzij: Dit wettelijk verplicht is We uw toestemming hebben Het nodig
           is voor het leveren van de app of het beheer van uw account{" "}
         </p>
         <p>
-          5. Gegevensoverdracht Uw gegevens kunnen worden overgedragen naar en
+          5. Gegevensoverdracht: Uw gegevens kunnen worden overgedragen naar en
           opgeslagen in landen buiten de Europese Economische Ruimte (EER). We
           nemen passende maatregelen om ervoor te zorgen dat uw gegevens
           beschermd zijn, ongeacht waar ze zich bevinden.
         </p>
         <p>
-          6.Gegevensbijwerken U kunt uw persoonlijke gegevens bijwerken of
+          6.Gegevensbijwerken: U kunt uw persoonlijke gegevens bijwerken of
           verwijderen door contact met ons op te nemen via [e-mailadres].
         </p>{" "}
         <p>
-          7. Gegeven beveiliging We implementeren passende technische en
+          7. Gegeven beveiliging: We implementeren passende technische en
           organisatorische maatregelen om uw gegevens te beschermen tegen
           ongeoorloofde toegang, gebruik, wijziging of vernietiging.  {" "}
         </p>
         <p>
-          8. Wijzigingen in het privacybeleid We kunnen dit privacybeleid van
+          8. Wijzigingen in het privacybeleid: We kunnen dit privacybeleid van
           tijd tot tijd bijwerken. We zullen u op de hoogte stellen van
           wijzigingen via de app of per e-mail.  
         </p>
         <p>
-          9. Contact Voor vragen of opmerkingen over ons privacybeleid kunt u
+          9. Contact: Voor vragen of opmerkingen over ons privacybeleid kunt u
           contact met ons opnemen via de livechat.
         </p>
       </article>
