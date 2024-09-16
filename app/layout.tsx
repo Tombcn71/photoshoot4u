2aa4dca8ecec157f86b9abe9712d7eaa1ddc85f5
Tom <tbvanreijn@gmail.com>
2024-09-16 12:04:07 +0200

    tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ const CrispWithNoSSR = dynamic(() => import("@/components/crisp"));
 
 import dynamic from "next/dynamic";
 import { CookieConsent } from "@/components/CookieConsent";
+import Script from "next/script";
 export const metadata = {
   title: "Professionele portretfoto's maken met AI✨ | AIfotosessie.nl",
   description:
@@ -18,6 +19,19 @@ export const metadata = {
 export default function RootLayout({ children }: any) {
   return (
     <html className="scroll-smooth" lang="en">
+      <head>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5PRW4T3L');
+`,
+          }}></Script>
+      </head>
       <body className="min-h-screen flex flex-col">
         <Banner />
         <CrispWithNoSSR /> <CookieConsent />
