6cef0ba187cadbd1d4bfbe11522ba422ba5435c5
Tom <tbvanreijn@gmail.com>
2024-09-16 13:17:06 +0200

    tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,12 @@ import Banner from "@/components/Banner";
 import "./globals.css";
 import { Suspense } from "react";
 import { Analytics } from "@vercel/analytics/react";
+import { GoogleTagManager } from "@next/third-parties/google";
 const CrispWithNoSSR = dynamic(() => import("@/components/crisp"));
 
 import dynamic from "next/dynamic";
 import { CookieConsent } from "@/components/CookieConsent";
-import Script from "next/script";
+
 export const metadata = {
   title: "Professionele portretfoto's maken met AI✨ | AIfotosessie.nl",
   description:
@@ -19,19 +20,11 @@ export const metadata = {
 export default function RootLayout({ children }: any) {
   return (
     <html className="scroll-smooth" lang="en">
-      <head>
-        <Script
-          id="gtm"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5PRW4T3L');
-`,
-          }}></Script>
-      </head>
+      <GoogleTagManager
+        gtmId="GTM-5PRW4T3L
+"
+      />
+
       <body className="min-h-screen flex flex-col">
         <Banner />
         <CrispWithNoSSR /> <CookieConsent />
